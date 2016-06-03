0661ea80f136274c13c29c2e0a567fff02fdadcc
Arthur Lai <arthur.lai93@gmail.com>
2016-06-02 23:07:10 -0700

    added protein package

---
+++ src/protein/AminoAcid.java
@@ -0,0 +1,15 @@
+package protein;
+
+public class AminoAcid {
+	
+	private String name;
+	private double molWeight;
+	private int category;
+	
+	public AminoAcid (String name, double molWeight, int category){
+		this.name = name;
+		this.molWeight = molWeight;
+		this.category = category;
+	}
+
+}
